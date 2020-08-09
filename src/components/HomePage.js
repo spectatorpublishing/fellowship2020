a35b299c67d28e8881ba0c3c920d1d17b35b13b6
Jennifer <wangxuanqing@hotmail.com>
2020-08-08 17:27:08 -0700

    got rid of right navbar; centered title + year

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,97 +1,76 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import whitemasthead from '../assets/whitemasthead.png';
-import { MobileAndTablet, Desktop } from 'react-responsive-simple';
+import React, { Component } from "react";
+import styled from "styled-components";
+import whitemasthead from "../assets/whitemasthead.png";
+import { MobileAndTablet, Desktop } from "react-responsive-simple";
 import "react-responsive-carousel/lib/styles/carousel.css";
-import { Carousel } from 'react-responsive-carousel';
-import HomepagePhotos from '../util/HomepagePhotos'
-import { NavHashLink } from 'react-router-hash-link';
-
+import { Carousel } from "react-responsive-carousel";
+import HomepagePhotos from "../util/HomepagePhotos";
+import { NavHashLink } from "react-router-hash-link";
 
 let MobileContainer = styled.div`
   width: 100vw;
   height: 100vh;
-`
-let HomePageContainer= styled(MobileContainer)`
+`;
+let HomePageContainer = styled(MobileContainer)`
   max-height: 65vw;
   display: flex;
   background-size: cover;
-`
+`;
 let LeftDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: flex-start;
-  @media(max-width:991px){
+  align-items: flex-center;
+  @media (max-width: 991px) {
     margin-top: 40vh;
     margin-left: -20vw;
   }
-`
-let LeftTitleDiv = styled.div`
-  margin-left: 10vw;
-`
-let LeftTitle = styled.h1`
+`;
+let TitleDiv = styled.div`
+  text-align: center;
+`;
+let Title = styled.h1`
   word-spacing: 100vw;
   line-height: 7rem;
-  color: ${props => props.theme.white};
-  text-shadow: ${props => props.theme.shadow};
-`
-let MobileLeftTitle = styled(LeftTitle)`
+  color: ${(props) => props.theme.white};
+  text-shadow: ${(props) => props.theme.shadow};
+`;
+let MobileLeftTitle = styled(Title)`
   margin-top: 1rem;
   line-height: 4rem;
-`
+`;
 let YearDiv = styled.div`
-  margin-left: 10vw;
-`
+  text-align: center;
+`;
 let Year = styled.h3`
-  color: ${props => props.theme.white};
-  text-shadow: ${props => props.theme.shadow};
-`
-let RightDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;;
-  align-items: flex-end;
-  padding-right: 5vw;
-  margin-right: 5vw;
-`
-let SectionTitleDiv = styled(NavHashLink)`
-  text-decoration: none;
-`
-let SectionTitle = styled.h3`
-  color: ${props => props.theme.white};
-  text-shadow: ${props => props.theme.shadow};
-  
-  &:hover {
-    color: ${props => props.theme.blue};
-    cursor: pointer;
-  }
-`
+  color: ${(props) => props.theme.white};
+  text-shadow: ${(props) => props.theme.shadow};
+`;
 let LogoDiv = styled.a`
   position: absolute;
   margin: 5vh 10vw;
   z-index: 1;
-  
+
   &:hover {
     opacity: 0.8;
   }
-`
+`;
 let SpecLogo = styled.img`
   height: 25px;
-`
+`;
 
 const Wrapper = styled.div`
-  @media(max-width:991px){
+  @media (max-width: 991px) {
     width: 100vw;
     overflow: hidden;
   }
-`
+`;
 
 let CarouselWrapper = styled.div`
   position: absolute;
   z-index: -1;
   width: 100vw;
-  @media(max-width:991px){
+  @media (max-width: 991px) {
     height: 100vh;
     width: auto;
     overflow: hidden;
@@ -100,22 +79,11 @@ let CarouselWrapper = styled.div`
       object-fit: cover;
     }
   }
-`
+`;
 
 export default class HomePage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-
-    this.getSections = this.getSections.bind(this);
-  }
-
-  getSections() {
-    
-    return this.props.menuItems.map((e, i) => 
-      <SectionTitleDiv key={i} to= {e.link}>
-        <SectionTitle>{e.name}</SectionTitle>
-      </SectionTitleDiv>
-    );
   }
 
   render() {
@@ -124,35 +92,33 @@ export default class HomePage extends Component {
         <MobileAndTablet>
           <MobileContainer>
             <HomePageContainer>
-            <Wrapper>
-            <CarouselWrapper>
-              <Carousel
-                showArrows={false}
-                showStatus={false}
-                showThumbs={false}
-                autoPlay={true}
-                infiniteLoop={true}
-                interval={4000}
-                showIndicators={false}
-                width={"100%"}
-              >
-                  {HomepagePhotos.map((el, i)=>
-                    <div key={i}>
-                      <img src={el} alt="carousel-i"/>
-                    </div>
-                  )}
-                </Carousel>
-              </CarouselWrapper>
+              <Wrapper>
+                <CarouselWrapper>
+                  <Carousel
+                    showArrows={false}
+                    showStatus={false}
+                    showThumbs={false}
+                    autoPlay={true}
+                    infiniteLoop={true}
+                    interval={4000}
+                    showIndicators={false}
+                    width={"100%"}
+                  >
+                    {HomepagePhotos.map((el, i) => (
+                      <div key={i}>
+                        <img src={el} alt="carousel-i" />
+                      </div>
+                    ))}
+                  </Carousel>
+                </CarouselWrapper>
               </Wrapper>
               <LeftDiv>
                 <YearDiv>
                   <Year>2020</Year>
                 </YearDiv>
-                <LeftTitleDiv>
-                  <MobileLeftTitle>
-                    Spectator Summer Fellowship
-                  </MobileLeftTitle>
-                </LeftTitleDiv>
+                <TitleDiv>
+                  <MobileLeftTitle>Spectator Summer Fellowship</MobileLeftTitle>
+                </TitleDiv>
               </LeftDiv>
             </HomePageContainer>
           </MobileContainer>
@@ -160,41 +126,35 @@ export default class HomePage extends Component {
 
         <Desktop>
           <LogoDiv href="https://www.columbiaspectator.com/" target="_blank">
-            <SpecLogo src={whitemasthead}/>
+            <SpecLogo src={whitemasthead} />
           </LogoDiv>
           <CarouselWrapper>
-              <Carousel
-                showArrows={false}
-                showStatus={false}
-                showThumbs={false}
-                autoPlay={true}
-                infiniteLoop={true}
-                interval={4000}
-                showIndicators={false}
-                width={"100%"}
-              >
-                  {HomepagePhotos.map((el, i)=>
-                    <div key={i}>
-                      <img src={el} alt="carousel-i"/>
-                    </div>
-                  )}
-                </Carousel>
-              </CarouselWrapper>
+            <Carousel
+              showArrows={false}
+              showStatus={false}
+              showThumbs={false}
+              autoPlay={true}
+              infiniteLoop={true}
+              interval={4000}
+              showIndicators={false}
+              width={"100%"}
+            >
+              {HomepagePhotos.map((el, i) => (
+                <div key={i}>
+                  <img src={el} alt="carousel-i" />
+                </div>
+              ))}
+            </Carousel>
+          </CarouselWrapper>
           <HomePageContainer>
             <LeftDiv>
               <YearDiv>
                 <Year>2020</Year>
               </YearDiv>
-              <LeftTitleDiv>
-                <LeftTitle>
-                  Spectator Summer Fellowship
-                </LeftTitle>
-              </LeftTitleDiv>
+              <TitleDiv>
+                <Title>Spectator Summer Fellowship</Title>
+              </TitleDiv>
             </LeftDiv>
-            
-            <RightDiv>
-              {this.getSections()}
-            </RightDiv>
           </HomePageContainer>
         </Desktop>
       </React.Fragment>
